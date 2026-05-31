fb6ea646bc4f2947471da1bcb42757dabd1480f0
Lucas Felix <luucasfeelix1925@hotmail.com>
2026-05-31 02:31:33 -0300

    realizado tela confirmaçao

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardapioComponent } from './pages/cardapio/cardapio.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { authGuard } from './core/guards/auth.guard';
+import { ConfirmacaoComponent } from './pages/confirmacao/confirmacao.component';
 
 export const routes: Routes = [
   {
@@ -16,8 +17,8 @@ export const routes: Routes = [
     component: CardapioComponent,
   },
   {
-  path: 'checkout',
-  component: CheckoutComponent,
+    path: 'checkout',
+    component: CheckoutComponent,
   },
   {
     path: 'admin/login',
@@ -28,8 +29,13 @@ export const routes: Routes = [
     component: DashboardComponent,
     canActivate: [authGuard],
   },
+    {
+    path: 'confirmacao',
+    component: ConfirmacaoComponent,
+  },
   {
     path: '**',
     redirectTo: 'cardapio',
   },
+
 ];
